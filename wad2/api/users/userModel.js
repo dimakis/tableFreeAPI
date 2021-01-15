93aa2843836fb141b7359e8e8ca3ef6b67d29f28
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2021-01-15 21:30:50 +0000

    trying to expand error handling and custom async validation

--- wad2/api/users/userModel.js
+++ wad2/api/users/userModel.js
@@ -3,16 +3,32 @@ import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 const UserSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+  // email: { type: String, unique: true, required: true },
+  email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+  password: { type: String, required: [true, 'Invalid password option'],min: 3 },
   id: { type: String, required: false },
-  first_name: { type: String, required: false },
-  last_name: { type: String, required: false },
-  username: { type: String, required: false },
-  status: { type: String, required: false },
-  isAdmin: { type: String, required: false },
-  //  favourites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movies'}]
+  first_name: { type: String },
+  last_name: { type: String },
+  username: { type: String },
+  status: { type: String },
+  privilegeLevel: { 
+    type: String, 
+    required: [true, 'Please input valid privilege level, 0 is default'], 
+    min:[0, 'Please enter correct privilege level: 0 (default) or higher'],  
+    max:[3,  'Please enter correct privilege level: 0 (default) - 3=max privilege level' ]},
 });
 
 // check user exists in db
@@ -43,16 +59,6 @@ UserSchema.methods.comparePassword = function(plaintext, cb) {
   }
   )
 }
-
-
-// UserSchema.methods.comparePassword = function(passw, cb) {
-//     bcrypt.compare(passw, this.password, (err, isMatch) => {
-//         if (err) {
-//             return cb(err);
-//         }
-//         cb(null, isMatch);
-//     });
-// };
 
 UserSchema.pre("save", function (next) {
   const user = this;
